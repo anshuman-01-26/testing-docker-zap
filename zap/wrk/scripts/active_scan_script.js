45d4d888a3ba22cd08a02b807dc9d67828f5a3e3
anshuman-01-26 <anshuman.mohapatra@maersk.com>
2022-10-11 10:32:10 +0530

    main logic

--- zap/wrk/scripts/active_scan_script.js
+++ zap/wrk/scripts/active_scan_script.js
@@ -21,13 +21,27 @@ function scan(as,msg,param,value){
         '[',
         ']'
     ]
-    var url = msg.getRequestHeader().getURI();
+    //var url = msg.getRequestHeader().getURI();
 
-    var new_url = new URL(url);
+    var new_url = new URL("https://github.com/anshuman-01-26?tab=repository&a=6");
     
     var param = new_url.searchParams;
     
     var entries = param.entries();
+
+    var search_quries = [];
+
+    for(var item of entries){
+        search_quries.push(item[0])
+    }
+    console.log(search_quries)
+    for(var i=0;i<injections.length;i++){
+        for(var j=0;j<search_quries.length;j++){
+            param.set(search_quries[j],injections[i])
+        }
+        console.log(new_url.searchParams)
+    }
+
 
 
     
